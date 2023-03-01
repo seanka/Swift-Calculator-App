37f3f9d3b658b15091563e9a9f3b8df6d82c30c0
seanka <and.seanka@gmail.com>
2023-03-01 10:15:18 +0700

    Connected each elements to own IBoutlet and IBaction

--- Calculator-App/ViewController.swift
+++ Calculator-App/ViewController.swift
@@ -9,11 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var displayLabel: UILabel!
+    
+    @IBAction func calcButtonPressed(_ sender: UIButton) {
     }
-
-
+    
+    @IBAction func numButtonPressed(_ sender: UIButton) {
+    }
+    
 }
 
