c813f451749b8005292fb73c2bd055d91ba21700
seanka <and.seanka@gmail.com>
2023-03-01 10:42:24 +0700

    Fixed button style to default, Added number button pressed logic

--- Calculator-App/ViewController.swift
+++ Calculator-App/ViewController.swift
@@ -11,10 +11,22 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var displayLabel: UILabel!
     
+    var isFinishedEditingNumber: Bool = true
+    
     @IBAction func calcButtonPressed(_ sender: UIButton) {
     }
     
     @IBAction func numButtonPressed(_ sender: UIButton) {
+        if let numValue = sender.currentTitle {
+            
+            if isFinishedEditingNumber {
+                displayLabel.text = numValue
+                isFinishedEditingNumber = false
+            } else {
+                displayLabel.text = displayLabel.text! + numValue
+            }
+            
+        }
     }
     
 }
